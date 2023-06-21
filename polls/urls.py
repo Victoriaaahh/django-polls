c9ad60666f927780c3b1c88285cb80dac49d9c60
Victoria Ayumi <victoriaayumikose@gmail.com>
2023-06-21 08:14:00 -0300

    adicionar o path no url e excluir python

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path("<int:question_id>/", views.detail, name="detail"),
 
     # ex: 127.0.0.1:8000/polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results")
+    path("<int:question_id>/results/", views.results, name="results"),
+
+    # ex: 27.0.0.1:8000/polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
 ]
 
--- polls/views.py
+++ polls/views.py
@@ -7,10 +7,10 @@ def index(request):
     return HttpResponse("Aqui serão apresentadas as últimas perguntas")
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta de número {question_id}".)
+    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
 
 def results(request, question_id):
-    return HttpResponse(f"Resultados da pergunta de número {question_id}".)
+    return HttpResponse(f"Resultados da pergunta de número {question_id}.")
 
 def vote(request, question_id):
-    return HttpResponse(f"Você vai votar na pergunta de número {question_id}".)
+    return HttpResponse(f"Você vai votar na pergunta de número {question_id}.")
