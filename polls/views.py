d3f2eeb89e4c5d65b8e2d28b7ede9ff6118e3b4f
Victoria Ayumi <victoriaayumikose@gmail.com>
2023-06-21 08:50:47 -0300

    criar o html e importar no views

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,12 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Aqui serão apresentadas as últimas perguntas")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"Essa é a pergunta de número {question_id}.")
@@ -14,3 +17,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"Você vai votar na pergunta de número {question_id}.")
+
+
