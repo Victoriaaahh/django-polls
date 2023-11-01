1d608c77705f4b1b2c46e05bf644d0ccb2092d65
Victoria <victoriaayumikose@gmail.com>
2023-11-01 09:34:12 -0300

    01/11

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
-
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 import datetime
 from django.utils import timezone
 
@@ -7,7 +9,14 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=300)
     pub_date = models.DateTimeField("date published", default=timezone.now)
-
+    author = models.ForeignKey(
+        User, 
+        editable = False
+        editable = False
+        on_delete = models.DO_NOTHING,
+        null= True
+    )
+    
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
